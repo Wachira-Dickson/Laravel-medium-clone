d936b25333542e01fc3eb6c8fe885992e5c1c26b
Your Name <dicksonwachira886@gmail.com>
2025-09-23 18:56:41 +0300

    Create a new post

---
+++ resources/views/post/create.blade.php
@@ -0,0 +1,47 @@
+<x-app-layout>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg p-8">
+
+                <form action="{{ route('post.store') }}" method="post">
+
+                    <!-- Image -->
+                    <div>
+                        <x-input-label for="image" :value="__('Image')" />
+                        <x-text-input id="image" class="block mt-1 w-full" type="file" 
+                        name="image" :value="old('image')" required>
+                        </x-text-input>
+                        <x-input-error :messages="$errors->get('image')" class="mt-2" />
+                    </div>
+
+
+
+                    <!-- Title -->
+                    <div class="mt-4">
+                        <x-input-label for="title" :value="__('Title')" />
+                        <x-text-input id="title" class="block mt-1 w-full" type="text" 
+                        name="title" :value="old('title')" required>
+                        </x-text-input>
+                        <x-input-error :messages="$errors->get('title')" class="mt-2" />
+                    </div>
+
+                    <!-- Content -->
+                    <div class="mt-4">
+                        <x-input-label for="content" :value="__('Content')" />
+                        <x-input-textarea id="content" class="block mt-1 w-full" type="text" 
+                        name="content" required> {{ old('content') }}
+                        </x-input-textarea>
+                        <x-input-error :messages="$errors->get('content')" class="mt-2" />
+                    </div>
+
+                    <x-primary-button class="mt-4">
+                        Submit
+                    </x-primary-button>
+
+                </form>
+
+
+            </div>
+        </div>
+    </div>
+</x-app-layout>
